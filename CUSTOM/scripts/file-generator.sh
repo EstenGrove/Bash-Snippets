6075cde3e3841ebe7c314ba35a8e501ac3e9316e
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-06-02 10:11:01 -0700

    Update file-generator.sh

--- CUSTOM/scripts/file-generator.sh
+++ CUSTOM/scripts/file-generator.sh
@@ -32,3 +32,11 @@ done
 
 # ONE LINER VERSION
 names=(test-1.txt test-2.txt test-3.txt); for name in "${names[@]}"; do rm -rf "$name"; done
+
+
+######### ALIASES FOR CREATING FILES OF A SPECIFIC SIZE #########
+
+alias numFiles='echo $(ls -1 | wc -l)'      # numFiles:     Count of non-hidden files in current dir
+alias make1mb='mkfile 1m ./1MB.dat'         # make1mb:      Creates a file of 1mb size (all zeros)
+alias make5mb='mkfile 5m ./5MB.dat'         # make5mb:      Creates a file of 5mb size (all zeros)
+alias make10mb='mkfile 10m ./10MB.dat'      # make10mb:     Creates a file of 10mb size (all zeros)
