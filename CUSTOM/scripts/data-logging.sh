d2ebd1e5f8dae2aabfec45829db47674b478117f
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-06-03 10:31:17 -0700

    Update data-logging.sh

--- CUSTOM/scripts/data-logging.sh
+++ CUSTOM/scripts/data-logging.sh
@@ -1,3 +1,8 @@
 # this script will count all references of "Object reference" and then write the value w/ a description to a log file
-# NOTE: "val" is the awk command as a variable. "$C" is the file to search thru as a variable
+
+
+
+# - "val" is the awk command as a variable. 
+# - "$C" is the file to search thru as a variable
+
 val=`(awk '/Object reference*/' $C | wc -l)`; echo "There are $val Object reference errors" > error-summary3.txt
