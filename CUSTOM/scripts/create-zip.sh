e8406ce6c1eb85b3fd6c7b414b7a7d2cf8d3cd91
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-06-03 10:38:24 -0700

    Update create-zip.sh

--- CUSTOM/scripts/create-zip.sh
+++ CUSTOM/scripts/create-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-zipf () { zip -r "$1".zip "$1" ; }          # zipf:         To create a ZIP archive of a folder
+zipf () { zip -r "$1".zip "$1" ; }          # zipf: To create a ZIP archive of a folder
