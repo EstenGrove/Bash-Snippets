fb6f31ba6ef9a17f47f432da389b74ebacf0971c
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-06-08 11:24:48 -0700

    Update backup-with-versioning.sh

--- CUSTOM/scripts/backups/backup-with-versioning.sh
+++ CUSTOM/scripts/backups/backup-with-versioning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# generates backup to a folder located ~/Backup and name the file related to the date processed
+# generates backup to a folder of your choosing and inlcude the date backed up
 
 backup() { source=$1 ; rsync --relative --force --ignore-errors --no-perms --chmod=ugo=rwX --delete --backup --backup-dir=$(date +%Y%m%d-%H%M%S)_Backup --whole-file -a -v $source/ ~/Backup ; } ; backup /source_folder_to_backup
